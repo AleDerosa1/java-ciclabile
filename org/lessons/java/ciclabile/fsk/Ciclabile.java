6a805c2df428cff39c8ff73a8819e9bbe1db7f78
AleDerosa1 <derosaalessandro018@gmail.com>
2025-05-22 00:16:55 +0200

    inizio esercizio

---
+++ org/lessons/java/ciclabile/fsk/Ciclabile.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Ciclabile {
+    public static void main(String[] args) {
+        ArrayList<Integer> interi = new ArrayList<>();
+        interi.add(10);
+        interi.add(73);
+        interi.add(54);
+        interi.add(19);
+        interi.add(12);
+        interi.add(22);
+        interi.add(89);
+        interi.add(47);
+        System.out.println(interi);
+    }
+
+}
